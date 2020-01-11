a749e3ad5aff63de03dd67e70aac4c03e881495d
Mayank Kumar <mayank22oct@gmail.com>
2020-01-11 22:18:16 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,20 @@
-wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-sh Anaconda3-2019.10-Linux-x86_64.sh
-echo "Enter the path of anaconda installation. In the format /home/<user_name>/anaconda3"
-echo -n ">>>"
-read LOC
-echo $LOC
-sudo $LOC/bin/pip install virtualenv virtualenvwrapper
+if which python3 > /dev/null 2>&1;
+then
+    #Python is installed
+	echo "Python3 found."
+else
+    #Python is not installed
+    echo "PYTHON3 NOT FOUND. INSTALLING."
+	sudo apt install python3 python3-pip
+fi
+LOC="$(which python3)"
+LOC="${LOC%/python3}"
+echo "$LOC"
+sudo pip install virtualenv virtualenvwrapper
+sudo pip install numpy scipy pandas matplotlib scikit-learn
 echo "export WORKON_HOME=\"\$HOME/.virtualenvs\"" >> $HOME/.bashrc
-echo "export VIRTUALENVWRAPPER_PYTHON=\"$LOC/bin/python\"" >> $HOME/.bashrc
-echo "export PATH=\"$LOC/bin:\$PATH\"" >> $HOME/.bashrc
-echo "source $LOC/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=\"$LOC/python3\"" >> $HOME/.bashrc
+echo "source $LOC/virtualenvwrapper.sh" >> $HOME/.bashrc
 source ~/.bashrc
 exec /bin/bash
 echo "Done"
